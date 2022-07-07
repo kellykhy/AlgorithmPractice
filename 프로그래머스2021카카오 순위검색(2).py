d2b8529198ef29eeb3510b22df0c5616fdc4da78
kellykhy <kellykhy@naver.com>
2022-07-07 16:04:38 +0900

    commit2

--- 프로그래머스2021카카오 순위검색(2).py
+++ 프로그래머스2021카카오 순위검색(2).py
@@ -8,13 +8,13 @@ def solution(infos, queries):
     answer = []
     info_dict = defaultdict(list) # info_dict의 default value는 리스트
     for info in infos:
-        info.split()
+        info = info.split()
         info_key = info[:-1]
         info_val = int(info[-1])
         for i in range(5):
             for c in combi(info_key, i):
                 tmp_key = ''.join(c)
-                info_dict[tmp_key] = info_val
+                info_dict[tmp_key].append(info_val)
     for key in info_dict.keys():
         info_dict[key].sort()
     
@@ -27,18 +27,24 @@ def solution(infos, queries):
             query.remove('and')
         while '-' in query:
             query.remove('-')
+
         tmp_q = ''.join(query)
         if tmp_q in info_dict:
             scores = info_dict[tmp_q] # list
             # query_score보다 같거나 큰 score의 인덱스 구하기
-            front, end = 0, len(scores) # 파이썬스러운 문법임!
-            while front < end:
-                mid = (front+end) // 2
-                if scores[mid] >= query_score:
-                    end = mid
-                else:
-                    start = mid+1
-            answer.append(len(scores) - start)
+            if len(scores) > 0:
+                front, end = 0, len(scores) # 파이썬스러운 문법임!
+                while front < end:
+                    mid = (front+end) // 2
+                    if scores[mid] >= query_score:
+                        end = mid
+                    else:
+                        front = mid+1
+                answer.append(len(scores) - front)
         else:
-            continue
+            answer.append(0)
     return answer
+
+info = ["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
+query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
+print(solution(info, query))
