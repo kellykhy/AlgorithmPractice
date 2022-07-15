e18252c0ec5efee5fcc1275884d52913b1ac10c9
kellykhy <kellykhy@naver.com>
2022-07-15 15:02:57 +0900

    commit2

--- 프로그래머스2018카카오 캐시 (LRU(Least Recently Used)).py
+++ 프로그래머스2018카카오 캐시 (LRU(Least Recently Used)).py
@@ -1,26 +1,25 @@
 # LRU(Least Recently Used) 구현  https://gomguard.tistory.com/115
-cities = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]
-cache_size = 2
-cache = [0] # cache[0]: 실행시간 저장
-def calc_cache(cache, string, cache_size):
-    try:
-        if len(cache) > cache_size: # exception: 1 > 0
-            if cache.count(string) == 0: #
-                cache.pop() #
-                cache[0] += 5 # exception-> error(except문으로 이동)
-            elif cache.count(string) == 1:
-                cache[0] += 1
-                cache.remove(string)
-        else:
-            if cache.count(string) == 0:
-                cache[0] += 5
-        cache.insert(1, string)
-    except:
-        cache[0] = 5 * len(cities)
-    finally:
-        return cache
+cities = ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]
+cacheSize = 3
 
-for city in cities:
-    cache = calc_cache(cache, city, cache_size)
+def solution(cacheSize, cities):
+    cache = [0] # cache[0]: 실행시간 저장
+    for city in cities:
+        try:
+            if len(cache) > cacheSize: # exception: 1 > 0
+                if cache.count(city) == 0: #
+                    cache.pop() #
+                    cache[0] += 5 # exception-> error(except문으로 이동)
+                elif cache.count(city) == 1:
+                    cache[0] += 1
+                    cache.remove(city)
+            else:
+                if cache.count(city) == 0:
+                    cache[0] += 5
+            cache.insert(1, city)
+        except:
+            cache[0] = 5 * len(cities)
+    answer = cache[0]
+    return answer
 
-print(cache[0])
+print(solution(cacheSize, cities))
