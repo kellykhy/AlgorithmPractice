9f6348cc366e2f6b7044cbacd6633b651fa06c32
kellykhy <kellykhy@naver.com>
2022-07-15 15:36:20 +0900

    commit3

--- 프로그래머스2018카카오 캐시 (LRU(Least Recently Used)).py
+++ 프로그래머스2018카카오 캐시 (LRU(Least Recently Used)).py
@@ -1,24 +1,28 @@
 # LRU(Least Recently Used) 구현  https://gomguard.tistory.com/115
-cities = ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]
-cacheSize = 3
+cities = ["Jeju", "Pangyo", "NewYork", "newyork"]
+cacheSize = 2
 
 def solution(cacheSize, cities):
     cache = [0] # cache[0]: 실행시간 저장
+    cities = [city.lower() for city in cities]
+    if cacheSize == 0:
+        answer = 5 * len(cities)
+        return answer
     for city in cities:
-        try:
-            if len(cache) > cacheSize: # exception: 1 > 0
-                if cache.count(city) == 0: #
-                    cache.pop() #
-                    cache[0] += 5 # exception-> error(except문으로 이동)
-                elif cache.count(city) == 1:
-                    cache[0] += 1
-                    cache.remove(city)
-            else:
-                if cache.count(city) == 0:
-                    cache[0] += 5
-            cache.insert(1, city)
-        except:
-            cache[0] = 5 * len(cities)
+        if len(cache) > cacheSize:
+            if cache.count(city) == 0:
+                cache.pop()
+                cache[0] += 5
+            elif cache.count(city) == 1:
+                cache[0] += 1
+                cache.remove(city)
+        else:
+            if cache.count(city) == 0:
+                cache[0] += 5
+            elif cache.count(city) == 1:
+                cache[0] += 1
+                cache.remove(city)
+        cache.insert(1, city)
     answer = cache[0]
     return answer
 
