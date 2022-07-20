e84e487f95d8bfb43323682233195bcc7f4244fe
kellykhy <kellykhy@naver.com>
2022-07-20 23:34:31 +0900

    initial commit1

--- 프로그래머스2019카카오 무지의 먹방 라이브 (2).py
+++ 프로그래머스2019카카오 무지의 먹방 라이브 (2).py
@@ -1,13 +1,6 @@
 # 프로그래머스2019카카오 무지의 먹방 라이브
 food_times = [4,1,1,1,1,1]
 k = 4
-print("original food_times :", food_times)
-
-def print_(t, food_times, zero_idx):
-    print("food_times:", food_times)
-    print("zero_dix:", zero_idx)
-    print("t:", t)
-    print()
 
 def find_zero(food_times):
     lst = []
@@ -31,16 +24,22 @@ def solution(food_times, k):
             food_times = [food_times + change for food_times, change in zip(food_times, change)]
             t += (len(food_times) - len(zero_idx))
             zero_idx = find_zero(food_times)
-            print_(t, food_times, zero_idx)
         else:
             for i in range(len(food_times)):
                 if i not in zero_idx:
                     if food_times[i]:
                         food_times[i] -= 1
                         t += 1
-                        print_(t, food_times, zero_idx)
-                    if t == k+1:
+                    if t == k + 1:
                         answer = i + 1
                         return answer
 
 print(solution(food_times, k))
+
+'''
+def print_(t, food_times, zero_idx):
+    print("food_times:", food_times)
+    print("zero_dix:", zero_idx)
+    print("t:", t)
+    print()
+'''
