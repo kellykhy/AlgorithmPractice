7991b00f69d9d597138f88b17136588ac6030d63
kellykhy <kellykhy@naver.com>
2022-07-21 00:32:18 +0900

    commit2

--- 프로그래머스2019카카오 무지의 먹방 라이브(3).py
+++ 프로그래머스2019카카오 무지의 먹방 라이브(3).py
@@ -9,19 +9,21 @@ def solution(food_times, k):
     if (sum(food_times) <= k):
         return -1
 
-    dict = {}
+    food_dict = {}
     length = len(food_times)
     for i in range(len(food_times)):
-        dict[i+1] = food_times[i]
+        food_dict[i+1] = food_times[i]
 
-    while (min(dict.values()) - cut) * length <= (k - t):
-        min_val = min(dict.values())
-        t += (min_val - cut) * length
-        cut = min_val
-        del dict[min(dict, key = dict.get)]
+    food_dict = sorted(food_dict.items(), key= lambda x : x[1])
+    while (food_dict[0][1] - cut) * length <= (k - t):
+        t += (food_dict[0][1] - cut) * length
+        cut = food_dict[0][1]
+        del food_dict[0]
         length -= 1
+        food_dict = sorted(food_dict, key= lambda x : x[1])
 
-    result = sorted(dict.items())
+    food_dict = dict(food_dict)
+    result = sorted(food_dict.items())
     answer = result[(k-t) % length][0]
 
     return answer
