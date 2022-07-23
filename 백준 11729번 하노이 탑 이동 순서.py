3465040917001c1456efc1c467de6ba361fbc191
kellykhy <kellykhy@naver.com>
2022-07-23 13:38:11 +0900

    commit 1

--- 백준 11729번 하노이 탑 이동 순서.py
+++ 백준 11729번 하노이 탑 이동 순서.py
@@ -1,2 +1,13 @@
 # 백준 11729번 하노이 탑 이동 순서
 
+def hanoi(n, depart, arrive, bypass):
+    if n == 1:
+        print(depart, arrive)
+    else:
+        hanoi(n-1, depart, bypass, arrive)
+        print(depart, arrive)
+        hanoi(n-1, bypass, arrive, depart)
+
+k = int(input())
+print((1<<k) - 1)
+hanoi(k, 1, 3, 2)
