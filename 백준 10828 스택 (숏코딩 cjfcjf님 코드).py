1af7d06dda0b89e6166b40e1658784ee79e29922
kellykhy <kellykhy@naver.com>
2022-07-28 23:02:54 +0900

    commit2

--- 백준 10828 스택 (숏코딩 cjfcjf님 코드).py
+++ 백준 10828 스택 (숏코딩 cjfcjf님 코드).py
@@ -3,10 +3,14 @@
 import sys
 s,p=[],print
 for a in sys.stdin.readlines()[1:]:
- a=a.strip()
- if 'z'in a:p(len(s))
- elif 'm'in a:p((not s)*1)
- elif 'h'in a:s+=int(a[5:]),
- else:
-  p(s[-1] if s else -1)
-  if a=='pop'and s:s.pop()
+    a=a.strip()
+    if 'z' in a: #size
+        p(len(s))
+    elif 'm' in a: #empty
+        p((not s)*1)
+    elif 'h' in a: #push
+        s+=int(a[5:]),
+    else:
+        p(s[-1] if s else -1) #pop, top
+        if a=='pop'and s:
+            s.pop()
