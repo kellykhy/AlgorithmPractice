446b47361135538dd46025c19d7451d357589221
Hui Yeong Kim <kellykhy@naver.com>
2022-09-13 13:45:27 +0900

    commit1

--- 프로그래머스 N으로 표현(2).py
+++ 프로그래머스 N으로 표현(2).py
@@ -7,13 +7,13 @@ def solution(N, number):
     answer = 0
     if N == number:                 # 테스트9 실패 요인
         return 1
-    possible_results = defaultdict(list)
+    possible_results = defaultdict(list) # value로 list를 취하는 딕셔너리/ {1:[5], 2:[55, ...], 3:[555, ...], ...}
     tmp = N
-    for i in range(1,9):
+    for i in range(1,9):  # 딕셔너리의 list들의 첫 원소는 5, 55, 555, 555... 등
         possible_results[i].append(tmp)
         tmp = tmp*10+N
-    for count in range(2,9):
-        for i in range(1, count//2+1):
+    for count in range(2,9): # count: N을 사용한 횟수
+        for i in range(1, count//2+1): # count = i + j/ count개의 N연산: i개의 N 연산 (연산) j개의 N 연산
             j = count - i
             for result1 in possible_results[i]:
                 for result2 in possible_results[j]:
@@ -28,7 +28,7 @@ def solution(N, number):
                     possible_results[count] += tmp_list
                     if number == possible_results[count][0] or number in tmp_list:
                         return count
-        possible_results[count][1:] = list(set(possible_results[count][1:]))
+        possible_results[count][1:] = list(set(possible_results[count][1:])) # list 내의 원소들 중복 제거
 
     return -1
 
