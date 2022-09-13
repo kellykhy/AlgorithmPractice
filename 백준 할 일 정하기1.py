21e5d568296b8a9ea68943ff9ce1e4ba6f62fa30
Hui Yeong Kim <kellykhy@naver.com>
2022-09-13 13:27:03 +0900

    commit1

--- 백준 할 일 정하기1.py
+++ 백준 할 일 정하기1.py
@@ -1,19 +1,20 @@
 # 백준 할 일 정하기1
 
-import sys
-input = sys.stdin.readline
 N = int(input())
-cost = [[*map(int, input().split())] for _ in range(N)]
-dp=[10**6]*(1<<N)
-dp[0]=0
+cost = []
+for i in range(N):
+    r = list(map(int, input().split()))
+    cost.append(r)
+dp = [10**6]*(1<<N)
+dp[0] = 0
 
 for i in range(1<<N):
     k = 0
-    for j in range(N):
+    for j in range(N): # k: 현재 작업 완료된 일의 수(=현재 작업에 투입할 작업자)
         if i & (1<<j):
             k+=1
     for j in range(N):
-        if i & (1 << j) == 0 and dp[i|(1<<j)] > dp[i]+cost[k][j]:
-            dp[i|(1<<j)] = dp[i] + cost[k][j]
+        if i & (1 << j) == 0:
+            dp[i|(1<<j)] = min(dp[i|(1<<j)], dp[i] + cost[k][j])
 
 print(dp[-1])
