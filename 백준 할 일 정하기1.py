27ae34215f8bf8a88986460454a81b06c118a1e1
Hui Yeong Kim <kellykhy@naver.com>
2022-09-13 14:20:30 +0900

    commit2

--- 백준 할 일 정하기1.py
+++ 백준 할 일 정하기1.py
@@ -1,16 +1,16 @@
 # 백준 할 일 정하기1
 
 N = int(input())
-cost = []
+cost = []                               # cost: 각 작업자(i)가 해당 일(j)을 수행하는데 드는 비용(D_ij) 저장
 for i in range(N):
     r = list(map(int, input().split()))
     cost.append(r)
-dp = [10**6]*(1<<N)
+dp = [10**6]*(1<<N)                     # dp: memoization
 dp[0] = 0
 
 for i in range(1<<N):
     k = 0
-    for j in range(N): # k: 현재 작업 완료된 일의 수(=현재 작업에 투입할 작업자)
+    for j in range(N):                  # k: 현재 작업 완료된 일의 수( = 현재 작업에 투입할 순서인 작업자)
         if i & (1<<j):
             k+=1
     for j in range(N):
