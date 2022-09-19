e9fd35dfacf2c6e99f25c0d9215a0833eb032d65
Hui Yeong Kim <kellykhy@naver.com>
2022-09-19 20:18:19 +0900

    try2 success

--- 프로그래머스 조이스틱.py
+++ 프로그래머스 조이스틱.py
@@ -1,5 +1,6 @@
-# 프로그래머스 조이스틱(시도1)
-# 채점결과: test13, test18, test20, test22, test24, test25, test27 실패
+# 프로그래머스 조이스틱(시도2번)
+# 시도1 채점결과: test13, test18, test20, test22, test24, test25, test27 -> 실패
+# 시도2 -> 통과
 
 # A: 아스키코드 65, Z: 아스키코드 122
 # name의 길이: 0 이상 20 이하
@@ -9,29 +10,31 @@ def solution(name):
     count = 0
     start, end = 0, 0
     pre = 0
-    min_case3 = 40 # 37
+    min_case3, min_case4 = 40, 40
     up_down = 0
     for i, l in enumerate(name):
         if (l!='A'):
             count+=1
-            if (count==1): start = i
-            else: min_case3 = min((pre*2+len(name)-i), min_case3)
+            if (count==1): 
+                start = i
+            else: 
+                min_case3 = min(pre*2+len(name)-i, min_case3)
+                min_case4 = min((len(name)-i)*2+pre, min_case4) # 시도1 실패요인 (빠뜨린 부분)
             pre = i
             if (ord(l)<78): up_down+=(ord(l)-65)
             else: up_down+=(90-ord(l)+1)
     end = pre
     case1 = end
     case2 = len(name)-start
-
-    answer = min(case1, case2, min_case3) + up_down
-    #print()
-    #print("case1:", case1)
-    #print("case2:", case2)
-    #print("min case3:", min_case3)
-    #print("start:", start)
-    #print("end:", end)
+    answer = min(case1, case2, min_case3, min_case4) + up_down
 
     return answer
 
+
+
 print(solution("AABAAAA")) #2+1=3
 print(solution("AABABBB")) #5+4=9
+print(solution("JEROEN")) #56
+print(solution("JAN")) #23
+print(solution("AAABBBABA")) #0
+print(solution("ABABAAAAABA")) #10 -> 시도1실패케이스
