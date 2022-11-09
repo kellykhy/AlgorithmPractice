d3f016fffef34da1285ad285d485f7fbe72364ed
Hui Yeong Kim <kellykhy@naver.com>
2022-11-09 17:13:18 +0900

    final final

--- 백준 1520번 내리막길(2).py
+++ 백준 1520번 내리막길(2).py
@@ -1,6 +1,9 @@
 # 백준 1520번 내리막길
 
 import sys
+import time
+start = time.time()  # 시작 시간 저장
+
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
@@ -12,10 +15,11 @@ def dfs(r,c):
         for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
             if (0<= r+dr <m and 0 <= c+dc < n and Map[r+dr][c+dc] < Map[r][c]):
                 dp[r][c] += dfs(r+dr, c+dc)
-    #print("dp[", r, "][", c, "] =", dp[r][c])
     return dp[r][c]
     
 m, n = map(int, input().split())
 Map = [list(map(int, input().split())) for _ in range(m)]
 dp = [[-1 for _ in range(n)] for _ in range(m)] # -1: 방문x/ 0: 방문o & 목표지점까지 갈 수 x/ etc: 방문o & 목표지점까지 가는 방법의 수
 print(dfs(0,0))
+
+print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
--- 백준 1520번 내리막길.py
+++ 백준 1520번 내리막길.py
@@ -1,27 +1,26 @@
 # 백준 1520번 내리막길(시간초과)
 
 import sys
+import time
+start = time.time()  # 시작 시간 저장
+
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-m, n = map(int, input().split())
-Map = []
-for r in range(m):
-    Map.append(list(map(int, input().split())))
-
 def dfs(r,c):
     global h
-    r_change = [0, 1, 0, -1]
-    c_change = [1, 0, -1, 0]
     if (r == m-1 and c == n-1):
         h += 1
         return
-    for i in range(4):
-        new_r = r + r_change[i]
-        new_c = c + c_change[i]
-        if (0 <= new_r < m and 0 <= new_c < n) and Map[new_r][new_c] < Map[r][c]:
-                #print(new_r, new_c)
-                dfs(new_r, new_c)
+    for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            if (0 <= r+dr <m and 0 <= c+dc < n and Map[r+dr][c+dc] < Map[r][c]):
+                dfs(r+dr, c+dc)
+    return
+
+m, n = map(int, input().split())
+Map = [list(map(int, input().split())) for _ in range(m)]
 h = 0
 dfs(0,0)
 print(h)
+
+print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
