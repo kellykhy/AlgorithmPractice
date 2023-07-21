7481e93c195db2fa03a2fdd4d811e2cf761857a4
kellykhy <kellykhy@naver.com>
2023-07-21 15:18:25 +0900

    initial commit

---
+++ 백준 2606번 바이러스.py
@@ -0,0 +1,21 @@
+# 2606번 바이러스
+
+n = int(input())
+e = int(input())
+graph = [[] for _ in range(n+1)] #인접 리스트
+visited = [0 for _ in range(n+1)]
+
+
+for i in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited[1] = 1
+def dfs(graph, n):
+    for i in graph[n]:
+        if (visited[i] == 0):
+            visited[i] = 1
+            dfs(graph, i)
+dfs(graph, 1)
+print(sum(visited) - 1)
