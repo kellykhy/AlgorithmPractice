5aa19b94a01de2361ed95ad796f904b752478a9f
kellykhy <kellykhy@naver.com>
2023-07-27 16:38:32 +0900

    Final

--- 백준 1707번 이분 그래프.py
+++ 백준 1707번 이분 그래프.py
@@ -1,50 +1,34 @@
-# 백준 1707번 이분 그래프
+# 백준 1707번 이분 그래프 (https://ji-gwang.tistory.com/293)
 
 import sys
 sys.setrecursionlimit(10 ** 6)
-
-# 입력받기
-nodes = []
-graphs = []
-
-k = int(sys.stdin.readline())
-
-for i in range(k):
-    v, e = [int(x) for x in sys.stdin.readline().split()]
-    graph = [[] for _ in range(v+1)]
-    node = []
-    for j in range(e):
-        a, b = [int(x) for x in sys.stdin.readline().split()]
-        graph[a].append(b)
-        graph[b].append(a)
-        node.append([a,b])
-    graphs.append(graph)
-    nodes.append(node)
-#print("nodes: ", nodes)
-
+input = sys.stdin.readline
 
 # dfs
-def dfs(graph, cur, visited, color):
-    visited[cur] = 1
-    check[cur] = color
-    #print(cur, end = ' ')
-    for i in graph[cur]:
-        if (visited[i]): continue
-        visited[i] = 1
-        dfs(graph, i, visited, color*(-1))
+def dfs(cur, color):
+    visited[cur] = color
+    for nxt in graph[cur]:
+        if (not visited[nxt]):
+            result = dfs(nxt, -color)
+            if (result == False):
+                return result
+        elif (visited[cur] == visited[nxt]):
+            return False
+    return True
 
-for i in range(k):
-    check = [0 for _ in range(len(graphs[i]))]
-    flag= 0
-    visited = [0 for _ in range(len(graphs[i]))]
-    dfs(graphs[i], 1, visited, 1)
-    #print(check)
-    flag = 0
-    for a, b in nodes[i]:
-        #print("a:", a, "b:", b)
-        if (check[a]+check[b] != 0):
-            flag = 1
-            break
-    if (flag == 1): print("NO")
-    else: print("YES")
-    check.clear()
+k = int(input())
+
+for _ in range(k):
+    v, e = map(int, input().split())
+    graph = [[] for _ in range(v+1)]
+    for j in range(e):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    visited = [False] * (v+1)
+    for i in range(1, v+1):
+        if not visited[i]:
+            result = dfs(i, 1)
+            if not result:
+                break
+    print("YES" if result else "NO")
