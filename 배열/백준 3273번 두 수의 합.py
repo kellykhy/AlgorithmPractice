d7ecca33da96e087f92f5b134be3035bd868e0d0
kellykhy <kellykhy@naver.com>
2024-03-24 16:30:54 +0900

    코드 수정

--- 배열/백준 3273번 두 수의 합.py
+++ 배열/백준 3273번 두 수의 합.py
@@ -2,13 +2,13 @@
 
 # input
 n = int(input())
-num_list = list(map(int, input().split(' ')))
+num_list = list(map(int, input().split()))
 x = int(input())
 
 result = 0
 ary = [0 for _ in range(1000001)]
 for num in num_list:
-    if (x-num > 0 and x-num < 1000001 and ary[x - num] > 0):
+    if (x - num > 0 and x - num < 1000001 and ary[x - num] > 0):
         result += 1
     ary[num] += 1
 
