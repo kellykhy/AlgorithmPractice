d9bf3a29e8f311b24d41de5f8a8f829817e22756
kellykhy <kellykhy@naver.com>
2024-04-18 23:23:45 +0900

    dp

--- DP/백준 11722번 가장 긴 감소하는 부분 수열.py
+++ DP/백준 11722번 가장 긴 감소하는 부분 수열.py
@@ -1,18 +1,15 @@
 # 백준 11722번 가장 긴 감소하는 부분 수열 
+
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 A = list(map(int, input().split()))
-dp = [0] * (N+1)
+dp = [1] * N
 
-for i in range(1, N+1):
-    max_val = 0
-    max_idx = 0
-    for k in range(i-1,0,-1):
-        if (A[-i] > A[-k] and dp[k] > max_val):
-            max_val = dp[k]
-            max_idx = k
-    dp[i] = dp[max_idx] + 1
+for i in range(0, N):
+    for j in range(i, N):
+        if (A[j] < A[i]):
+            dp[j] = dp[i] + 1 # dp[i] : i번째 포함한 수열의 길이의 최댓값
 
-print(max(dp))
+print(dp)
