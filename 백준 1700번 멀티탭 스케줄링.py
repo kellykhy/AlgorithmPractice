da2e0f97e8d39a941b428d467dc2bd3f81f52142
kellykhy <kellykhy@naver.com>
2024-07-02 14:22:33 +0900

    greedy

--- 백준 1700번 멀티탭 스케줄링.py
+++ 백준 1700번 멀티탭 스케줄링.py
@@ -5,40 +5,26 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 order = list(map(int, input().split()))
-currentMultitap = [0 for _ in range(K+1)]
+multitap = []
 result = 0
-
-plugs = 0
+        
 for i in range(K):
-    if plugs >= N:
-        break
-    if not currentMultitap[order[i]]:
-        currentMultitap[order[i]] += 1
-        plugs += 1
-        
-for i in range(N, K):
-    if currentMultitap[order[i]]:
+    if order[i] in multitap:
         continue
-    elif (sum(currentMultitap) < N):
-        currentMultitap[order[i]] += 1
+    if (len(multitap) < N):
+        multitap.append(order[i])
         continue
-    else:
-        toBeUsed = []
-        for j in range(i + 1, K):
-            if order[j] in toBeUsed:
-                continue
-            if currentMultitap[order[j]] == 1:
-                toBeUsed.append(order[j])
-            if len(toBeUsed) == N - 1:
-                break
-        
-        plug_to_remove = 1
-        for plug in range(1, K+1):
-            if (currentMultitap[plug] and plug not in toBeUsed):
-                plug_to_remove = plug
-                break
-        currentMultitap[plug_to_remove] -= 1
-        currentMultitap[order[i]] += 1
-        result += 1    
+    
+    toBeUsed = []
+    for plug in multitap:
+        if plug in order[i+1:]:
+            toBeUsed.append(order[i+1:].index(plug))
+        else:
+            toBeUsed.append(101)
+            
+    plug_to_remove = toBeUsed.index(max(toBeUsed))
+    multitap.remove(multitap[plug_to_remove])
+    multitap.append(order[i])
+    result += 1
     
 print(result)
