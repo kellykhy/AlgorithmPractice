c264a45df03a4a1d23e301793e82714b920f87d3
kellykhy <kellykhy@naver.com>
2024-09-08 18:04:35 +0900

    add one line

--- 그리디/백준 7570번 줄 세우기.py
+++ 그리디/백준 7570번 줄 세우기.py
@@ -1,6 +1,6 @@
 # 백준 7570번 줄 세우기
 
-# idea: 안움직여도 되는 것의 개수의 최댓값은 = 연속된 숫자(오름차순)의 최대길이
+# idea: 안 움직여도 되는 최댓값은 = 연속된 숫자(오름차순)의 최대길이
 # n - (연속된 수의 최대길이)
 
 import sys
@@ -20,4 +20,5 @@ for i in range(2,n+1):
     else:
         max_length = max(max_length, length)
         length = 1
+max_length = max(max_length, length)
 print(n-max_length)
