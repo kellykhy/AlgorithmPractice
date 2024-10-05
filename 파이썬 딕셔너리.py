d157e8d2ee906b53eb768f6da2a88ac310938824
kellykhy <kellykhy@naver.com>
2024-10-05 14:55:30 +0900

    data structure(python)

---
+++ 파이썬 딕셔너리.py
@@ -0,0 +1,9 @@
+# 딕셔너리 자료구조
+
+d = {1: "apple", 2: "grapes", 3: "banana", 4: "pineapple"}
+sorted_d1 = sorted(d.items())
+sorted_d2 = dict(sorted(d.items()))
+
+print(d)
+print(sorted_d1)
+print(sorted_d2)
