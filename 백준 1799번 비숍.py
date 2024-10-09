965d77e38e89e8089e7ef133dd53d7c0f2df85f5
kellykhy <kellykhy@naver.com>
2024-10-09 13:26:34 +0900

    global 처리

--- 백준 1799번 비숍.py
+++ 백준 1799번 비숍.py
@@ -7,13 +7,15 @@ input = sys.stdin.readline
 N = int(input())
 chess = []
 for _ in range(N):
-    row = list(map(int, input().split()))
+    row = list(map(int, input().split()))   
     chess.append(row)
 
-# solution 
-def white(N, chess, white_leftdiag, n, cnt, white_cnt):
+# solution
+
+def white(N, chess, white_leftdiag, n, cnt):
+    global white_cnt
     if n == N:
-        white_cnt[0] = max(white_cnt[0], cnt)
+        white_cnt = max(white_cnt, cnt)
         return
     flag = 1
     for i in range(-N//2, N//2+1):
@@ -23,13 +25,14 @@ def white(N, chess, white_leftdiag, n, cnt, white_cnt):
                     continue
                 white_leftdiag[i] = 1
                 flag = 0
-                white(N, chess, white_leftdiag, n+1, cnt+1, white_cnt)
+                white(N, chess, white_leftdiag, n+1, cnt+1)
                 white_leftdiag[i] = 0
-    if flag: white(N, chess, white_leftdiag, n+1, cnt, white_cnt)
+    if flag: white(N, chess, white_leftdiag, n+1, cnt)
     
-def black(N, chess, black_leftdiag, n, cnt, black_cnt):
+def black(N, chess, black_leftdiag, n, cnt):
+    global black_cnt
     if n == N:
-        black_cnt[0] = max(black_cnt[0], cnt)
+        black_cnt = max(black_cnt, cnt)
         return
     flag = 1
     for i in range(-N//2, N//2):
@@ -39,17 +42,18 @@ def black(N, chess, black_leftdiag, n, cnt, black_cnt):
                     continue
                 black_leftdiag[i] = 1
                 flag = 0
-                black(N, chess, black_leftdiag, n+1, cnt+1, black_cnt)
+                black(N, chess, black_leftdiag, n+1, cnt+1)
                 black_leftdiag[i] = 0
-    if flag: black(N, chess, black_leftdiag, n+1, cnt, black_cnt)
-
+    if flag: black(N, chess, black_leftdiag, n+1, cnt)
+    
 def solution(N, chess):
-    white_cnt = [0] # call by reference는 mutable 자료형에 대해서만 가능하기 때문에 이렇게 해줌
-    black_cnt = [0] # 위와 마찬가지로 call by reference를 위해 자료형을 리스트로 함.
+    global white_cnt, black_cnt
+    white_cnt = 0
+    black_cnt = 0
     white_leftdiag = {i:0 for i in range(-N//2, N//2+1)}
     black_leftdiag = {i:0 for i in range(-N//2, N//2)}
-    white(N, chess, white_leftdiag, 0, 0, white_cnt)
-    black(N, chess, black_leftdiag, 0, 0, black_cnt)
-    return white_cnt[0] + black_cnt[0]
+    white(N, chess, white_leftdiag, 0, 0)
+    black(N, chess, black_leftdiag, 0, 0)
+    return white_cnt + black_cnt
 
 print(solution(N, chess))
