93c8bae8938b1322338e46c00c5a2148e32188cf
kellykhy <kellykhy@naver.com>
2025-03-13 23:53:48 +0900

    two_pointer & accumulative sum

---
+++ 백준 2283번 구간 자르기.py
@@ -0,0 +1,33 @@
+# 백준 2283번 구간 자르기
+
+import sys
+input = sys.stdin.readline
+
+max_value = 1000000
+N, K = map(int, input().split())
+arr = [0] * (max_value+2) # 0 ~ 1000001
+
+for _ in range(N):
+    s, e = map(int, input().split())
+    arr[s+1] += 1
+    arr[e+1] -= 1
+for i in range(1,1000002):
+    arr[i] += arr[i-1]
+  
+s, e = 0, 0
+flag = False
+value = 0
+while (s <= e <= max_value):
+    if value < K:
+        e += 1
+        value += arr[e]
+    elif value > K:
+        s += 1
+        value -= arr[s]
+    else:
+        flag = True
+        break
+if flag:
+    print(s, e)
+else:
+    print("0 0")
